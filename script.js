e4f70eb3db0546bbc066c4fe31184eb05855528d
quang <quangphan@gmail.com>
2025-06-03 01:13:46 +0700

    Mô tả ngắn gọn thay đổi

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const messages = [
     "Moah moah moah"
   ];
   
-  const images = ["1.JPG", "2.JPG", "3.JPG", "4.JPG", "5.JPG"];
+  const images = ["images/1.JPG", "images/2.JPG", "images/3.JPG", "images/4.JPG", "images/5.JPG"];
   
   const container = document.getElementById("container");
   
